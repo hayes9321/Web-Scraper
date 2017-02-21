aa19ffe52f112729d94b8ba5ec94689691cd9e1e
hayes9321 <t.hayes8321@gmail.com>
2017-02-21 14:06:36 -0800

    web-scraper v1 github upload

---
+++ README.md
@@ -0,0 +1,11 @@
+#Web Scraper
+
+This is a fun project that allows web scraping from the command line, utilizing the node.js framework.
+
+##How to use - 
+
+      git clone repository
+      npm install
+
+After you have cloned the repository open up the command line and navigate to the folder. Run the command: **node index.js [name of your search]**
+to begin scraping google and bing for the top 6 search results.
---
+++ index.js
@@ -0,0 +1,110 @@
+/*
+  Check list
+
+  In NodeJS, please write a script that takes a keyword as an argument from the command line.  
+  The script would then search Google and Bing for the keyword.   
+  The script will visit the top 6 search results from both Google and Bing.   
+  On each visited page from the search results list, scrape the <title> element.  
+  Store the url for the visited page and the contents of the <title> element in a CSV file.   
+  In the end there should be two CSV files output:  one for the Google results and one for Bing.
+*/
+
+
+//Require Dependencies
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var express = require('express');
+var app = express();
+
+//Make sure the user passing in an argument to run program
+if(process.argv.length > 2){
+  runScrape();
+} else{
+  console.log('Please enter an argument after node index.js');
+}
+
+//Function that runs the search engine scrape
+function runScrape(){
+  //Pass the argument from the command line to the runScrape scope.
+  var keyword = process.argv.slice(2);
+
+  //Scrape Google's Search Engine
+  request('https://www.google.com/search?q=' + keyword, function (error, response, html) {
+    if(!error && response.statusCode == 200){
+
+      var $ = cheerio.load(html);
+      var googleSearchResults = [];
+      var totalResults = 0;
+      var links = $('h3.r a');
+      
+      links.each(function(index, element){
+        //return the top 6 results
+        if(totalResults <= 5){
+          //get href
+          var url = $(element).attr('href');
+          
+          //clean up returned url
+          url = url.replace("/url?q=", "").split("&")[0];
+
+          if(url.charAt(0) === '/'){
+            return;
+          }
+          
+          totalResults++;
+
+          googleSearchResults.push({
+            title: $(element).text(),
+            url: url
+          });
+        }
+      });
+      
+      var jsonData = JSON.stringify(googleSearchResults, null, 4);
+      //write the entire scraped page to the local file system
+      fs.writeFile(__dirname + '/google-output.json', jsonData, function(err){
+        console.log('File successfully written! - Check your google-output.json for the output file');
+      })
+    
+    } 
+  });
+
+  //Scrape Bing's Search Engine
+  request('https://www.bing.com/search?q=' + keyword, function (error, response, html) {
+    if(!error && response.statusCode == 200){
+      var $ = cheerio.load(html);
+      var bingSearchResults = [];
+      var totalResults = 0;
+      var links = $(' h2 a');
+
+      links.each(function(index, element){
+        if(totalResults <= 5){
+          //get href
+          var url = $(element).attr('href');
+          //clean up returned url
+
+          url = url.replace("/url?q=", "").split("&")[0];
+
+          if(url.charAt(0) === '/'){
+            return;
+          }
+          
+          totalResults++;
+
+          //push the object
+          bingSearchResults.push({
+            title: $(element).text(),
+            url: url
+          });
+        }
+      });
+      
+      var jsonData = JSON.stringify(bingSearchResults, null, 4);
+      //write the entire scraped page to the local file system
+      fs.writeFile(__dirname + '/bing-output.json', jsonData, function(err){
+        console.log('File successfully written! - Check your bing-output.json for the output file');
+      })
+    
+    } 
+  });
+}
